b635c129e04441933d2907808b3cc5cd285f3b11
Ravi Babu Pathuri <ravibpathuri@gmail.com>
2022-05-24 22:49:33 +0530

    Authentication added

---
+++ components/LoginButton.tsx
@@ -0,0 +1,32 @@
+import { Button, HStack, Stack } from "@chakra-ui/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        {session?.user?.name}
+        <br />
+        {session?.user?.image}
+        <br />
+        <img src={session?.user?.image} alt="" />
+        <HStack>
+          <Button colorScheme="red" onClick={() => signOut()}>
+            Sign out
+          </Button>
+        </HStack>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <HStack>
+        <Button colorScheme="green" onClick={() => signIn()}>
+          Login
+        </Button>
+      </HStack>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { appWithTranslation } from "next-i18next";
+import { SessionProvider } from "next-auth/react";
 
 import { reactPlugin } from "../AppInsights";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <AppInsightsContext.Provider value={reactPlugin}>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
       </ChakraProvider>
     </AppInsightsContext.Provider>
   );
