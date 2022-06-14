e0076148c16d27515bde572e118347b192bca047
Mourya Modugula <52524065+mouryamodugulamm@users.noreply.github.com>
2022-06-14 13:06:27 +0530

    PWA -removed

--- next.config.js
+++ next.config.js
@@ -9,13 +9,3 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-const withPWA = require("next-pwa");
-module.exports = withPWA({
-  //...before
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-  //...after
-});
